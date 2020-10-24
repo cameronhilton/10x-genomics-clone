843628213d509c374f20a26ecec620a4d8f3415d
Cameron Hilton <cameronhilton@gmail.com>
2020-10-24 16:23:15 -0700

    basic Header component

---
+++ components/Header.jsx
@@ -0,0 +1,17 @@
+import styles from '../styles/Header.module.scss'
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>10X GENOMICS</div>
+      <div className={styles.menuItems}>
+        <div>Products</div>
+        <div>Research Areas</div>
+        <div>Resources</div>
+        <div>Support</div>
+        <div>Company</div>
+        <div>Careers</div>
+      </div>
+    </div>
+  );
+};

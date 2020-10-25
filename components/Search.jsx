a08f50e07e818e59073468c47833f22ffb1c02fe
Cameron Hilton <cameronhilton@gmail.com>
2020-10-24 21:58:03 -0700

    add basic Search component

---
+++ components/Search.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from '../styles/Search.module.scss'
+
+export default class Search extends React.Component {
+  state = {
+    query: '',
+  }
+
+  render () {
+    const { query } = this.state;
+
+    return (
+      <div className={styles.search}>
+        <form>
+          <span className={styles.searchIcon}>&#128269;</span>
+          <input
+            className={styles.searchBox}
+            type='search'
+            name='Publication search'
+            placeholder='Search abstracts, sample types, and more'
+            value={query}
+            onChange={(e) => this.setState({query: e.target.value})}
+          />
+          {
+            query && <span className={styles.clearIcon} onClick={() => this.setState({query: ''})}>X</span>
+          }
+        </form>
+      </div>
+    );
+  }
+
+};
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import Search from '../components/Search';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.main}>
       <Head>
         <title>10x Genomics Clone</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,19 +14,13 @@ export default function Home() {
       <Header />
 
       <main className={styles.main}>
+        <div className={styles.titleContainer}>
+          <div className={styles.pageTitle}>Publications</div>
+        </div>
 
+        <Search />
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 };

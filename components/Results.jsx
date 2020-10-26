b4820f89a2db0f2d48bd2999bf08cf0a5c3c3c5f
Cameron Hilton <cameronhilton@gmail.com>
2020-10-25 22:14:53 -0700

    add initial facets to results filter

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,10 +1,33 @@
+import React from 'react';
 import withData from './withData';
 import styles from '../styles/Results.module.scss'
+
+const FacetList = ({ facets, title }) => {
+  return (
+    <div>
+      <div className={styles.facetType}>{title}</div>
+      <div className={styles.facetTypeContainer}>
+        {
+          Object.keys(facets).map((facet) => (
+            <div className={styles.facetType}>{facet} ({facets[facet]})</div>
+          ))
+        }
+      </div>
+    </div>
+  );
+};
 
 const Results = ({ data, dataSource }) => (
   <div className={styles.results}>
     <div className={styles.stats}>
-      <div>{data[0].nbHits} RESULTS</div>
+      <section className={styles.facets}>
+        <FacetList facets={data[0].facets.tags} title='TAGS' />
+        <FacetList facets={data[0].facets.species} title='SPECIES' />
+        <FacetList facets={data[0].facets.tissueTypes} title='TISSUE TYPE' />
+        <FacetList facets={data[0].facets.journal} title='JOURNAL' />
+        <FacetList facets={data[0].facets.productGroups} title='10X GENOMICS PRODUCT' />
+      </section>
+      <section>{data[0].nbHits} RESULTS</section>
     </div>
   </div>
 );
